cbbae05091eb9c492b8b422883e04ee2f2b7badb
Kalle842 <kalle.freigie@elev.ga.ntig.se>
2023-01-24 15:38:25 +0100

    Update index.php

--- algoritmer/upgift 4/index.php
+++ algoritmer/upgift 4/index.php
@@ -9,13 +9,13 @@
 <body>
     <?php
     $n = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14 ,15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32);// varaibel för en array med 
-    function middle(){
+    function middle($n){
         $d = array_sum($n);// variabel som innehåller värdet på alla numer i arrays värde tillsamans
         $c = count($n); // variabel för att räkna antalet siffror
         $f = $d / $c; // variabel för vad d delat på c blir på hemsidan
-        return $f
-    };
-    echo implode($f);
+        return $f;
+    }
+    echo middle($n);
     ?>
 </body>
 </html>
