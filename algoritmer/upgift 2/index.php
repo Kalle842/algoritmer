7e866553bbc500beead5bf7ae1a0eb9b8f9feeff
Kalle842 <kalle.freigie@elev.ga.ntig.se>
2023-01-24 12:55:46 +0100

    adsf

--- algoritmer/upgift 2/index.php
+++ algoritmer/upgift 2/index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>upgift 2</title>
 </head>
 <body>
     <?php
-    $a = rand(0, 100);
-    $b = 5;
-    echo $a + $b;
+    $a = rand(0, 100);// variabel som väljer ett numer mellan 0-100
+    $b = 5;// variabel med värdet 5
+    echo $a + $b;// skriver ut variablerna adderade med varandra
     ?>
 </body>
 </html>
