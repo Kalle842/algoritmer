9b9a1571441407bccd103ef37e637149a7063c62
Kalle842 <kalle.freigie@elev.ga.ntig.se>
2023-01-24 14:39:06 +0100

    Update index.php

--- algoritmer/upgift 3/index.php
+++ algoritmer/upgift 3/index.php
@@ -9,17 +9,20 @@
 <body>
     <?php 
         function factorialtal($num){
-            if($num <= 1){ // kollar ifall $num är större eller lika med 1
+            if($num <= 1){ // kollar ifall $num är större eller lika med 1 ifall den är det så skickar den 1
                 return 1;
             }
-                  else  
-                { 
-                for($x=$num; $x >= 1; $x++){
-                    $num = $num * $x;
-                };
+            else  // ifall inte if satsen stämmer så kommer den köra följande for loop
+            { 
+                for($x=$num-1;$x > 1; $x--){/* denna for loop säger att x är lika med (num - 1) och det 
+                    loopas tills x fortfarande är större än ett medans num också gångas med alla värden på x 
+                    då x är större än ett  */ 
+                    $num *= $x;// num gångas med alla värden på x då x är större än 1
+                }
             }
-        };
-        echo factorialtal(5);
+            return $num;// skickar vad $num blir till slut
+        }
+        echo factorialtal(5);// visar upp resulatet av function på hemsidan
     ?> 
 </body>
 </html>
